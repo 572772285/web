e7d5ac6c40b95898da2ec829030ce7ccce2086dd
yang <572772285@qq.com>
2018-08-22 16:10:53 +0800

    8-22

--- 22-react/05-server/server.js
+++ 22-react/05-server/server.js
@@ -2,7 +2,7 @@ const http=require('http');
 
 const server = http.createServer((req,res)=>{
 res.setHeader('Access-Control-Allow-Origin','*');
-	let data=['xuexi','nuli']
+	let data=['i love study','i love you']
 	res.end(JSON.stringify(data))
 })
 server.listen(3001,'127.0.0.1',()=>{
